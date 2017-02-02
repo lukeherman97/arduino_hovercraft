ae580e0cf7cb2282cbcda6eec2973a5608a6d7df
andrewkuqel <andrew.g.nicholson@gmail.com>
2017-02-02 14:20:56 -0800

    Update line_follower.ino

--- Target_Detection/line_follower.ino
+++ Target_Detection/line_follower.ino
@@ -1,12 +1,30 @@
-int input = A0;
-int value;
 void setup(){
   Serial.begin(9600);
-  pinMode(A0, INPUT);
-
+  int TopPin = A0;
+  int RightPin = A1;
+  int BottomPin = A2;
+  int LeftPin = A3;
+  
+  pinMode(TopPin, INPUT);
+  pinMode(RightPin, INPUT);
+  pinMode(BottonPin, INPUT);
+  pinMode(LeftPin, INPUT);
+  int TopValue, RightValue, BottomValue, LeftValue, TopLed, RightLed, BottomLed, LeftLed;
 }
+
 void loop(){
-  value=analogRead(A0);
-  Serial.println(value);
-  delay(200);
+  TopValue = analogRead(TopPin)
+  RightValue = analogRead(RightPin)
+  BottomValue = analogRead(BottomPin)
+  LeftValue = analogRead(LeftPin)
+
+  if(TopValue<100){
+   digitalWrite(TopLed, HIGH)
+  }
+  else{
+  digitalWrite(TopLed, LOW)
+  }
+  if(BottomValue<100){
+	digitWrite(BottomLed, HIGH)
+  }
 }
